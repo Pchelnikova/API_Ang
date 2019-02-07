c81a0d460002bd3e7de42969e96eaaa1e8976d59
Iryna <pchelnikova23@gmail.com>
2019-02-07 15:38:33 +0200

    Add project files.

---
+++ API_Ang_BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace API_Ang_BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ API_Ang_DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace API_Ang_DAL
+{
+    public class Class1
+    {
+    }
+}
